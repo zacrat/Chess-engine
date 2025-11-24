9e915b8a6781b64fc9c1f42802f8c3657c51c90b
zacrat <zackrat45@gmail.com>
2025-11-24 15:52:05 +0000

    Pawn Fix again

--- Main.py
+++ Main.py
@@ -7,8 +7,6 @@ white.enemy = black
 black.enemy = white
 
 running = True
-Start = Game.Game(white, 2, "R##Q#R#K##PB##P#P###P#b##P#Pp#P#######q###p#p###pp###pppr###k##r")
+Start = Game.Game(white, 2)
 while running:
     running = Start.play()
-
-#unable to move bishop
--- Pieces.py
+++ Pieces.py
@@ -50,6 +50,8 @@ class pawn(piece):
                 ToCheckY = ToCheckBlackY
             if not isinstance(board[y+ToCheckY[0]][x], piece):
                 self.posMove.append([y+ToCheckY[0],x])
+                if not isinstance(board[y+2(ToCheckY[0])][x], piece):
+                    self.posMove.append([y+2(ToCheckY[0]),x])
             for i in range(2):
                 if 0<y+ToCheckY[i]<8 and 0<x+ToCheckX[i]<8:
                     checking = board[y+ToCheckY[i]][x+ToCheckX[i]]
