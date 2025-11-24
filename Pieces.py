9ae181e47f0ad629052597d4cc29573683abd38c
zacrat <zackrat45@gmail.com>
2025-11-24 16:12:15 +0000

    Pawns fixed (Hopefully)

--- Pieces.py
+++ Pieces.py
@@ -45,13 +45,15 @@ class pawn(piece):
             if isinstance(self.colour, Sides.WhiteSide):
                 PosVal = self.mapWhite[y][x]
                 ToCheckY = ToCheckWhiteY
+                startLevel = 6
             else: 
                 PosVal = self.mapBlack[y][x]
                 ToCheckY = ToCheckBlackY
+                startLevel = 1
             if not isinstance(board[y+ToCheckY[0]][x], piece):
                 self.posMove.append([y+ToCheckY[0],x])
-                if not isinstance(board[y+2(ToCheckY[0])][x], piece):
-                    self.posMove.append([y+2(ToCheckY[0]),x])
+                if not isinstance(board[y+2*(ToCheckY[0])][x], piece)  and y == startLevel:
+                    self.posMove.append([y+2*(ToCheckY[0]),x])
             for i in range(2):
                 if 0<y+ToCheckY[i]<8 and 0<x+ToCheckX[i]<8:
                     checking = board[y+ToCheckY[i]][x+ToCheckX[i]]
