2b36ece9a3d8129a25eecd695c8ac6609f063117
zacrat <zackrat45@gmail.com>
2026-02-04 15:27:35 +0000

    Few fixes, implimentation of prev moves tracking.

--- Main.py
+++ Main.py
@@ -16,12 +16,21 @@ def game_loop(game):
     while game.ongoing:
         game.play()
 
+def open_settings():
+    pass
+
 def play_game():
     Start = Game.Game(white, 2)
+    LTN = ["a","b","c","d","e","f","g","h"]
+    nums = [8,7,6,5,4,3,2,1]    
+    if Start.bot.side == black:
+        nums = nums.reverse()
+        LTN = LTN.reverse()
     game_thread = threading.Thread(target=game_loop, args=(Start,), daemon=True)
     game_thread.start() 
     selected_square = None
     possible_moves = []
+    prev_moves = []
     while Start.ongoing:
         cur_pos = Start.curPos
         for event in pygame.event.get():
@@ -31,28 +40,35 @@ def play_game():
                 mouse_x, mouse_y = pygame.mouse.get_pos()
                 col = (mouse_x // SQUARE_SIZE)
                 row = (mouse_y // SQUARE_SIZE)
+                move_found = False
                 for move in possible_moves:
                     from_pos = (selected_square[0], selected_square[1])
                     if len(move) > 2:# [What rook to move, Where to move the king, Where to move the rook] 
                         if move[1] == [row, col]:                   
                             Start.player_queue.put((move[0], move[1], move[2], from_pos))
+                            prev_moves.append((from_pos, move[1]))
                             selected_square = None
                             possible_moves = [] 
+                            move_found = True
                             break
                     else:
-                        r, c = move
-                        if [r, c] == [row, col] and selected_square != None:
+                        if move == [row,col]:
                             to_pos = (row, col)
                             Start.player_queue.put((from_pos, to_pos))
+                            prev_moves.append((from_pos, to_pos))
                             selected_square = None
                             possible_moves = []
-                            break           
+                            move_found = True
+                            break 
+                if move_found:
+                    break
                 if [row, col] == selected_square:
                     selected_square = None
                     possible_moves = []
                 else:
                     selected_square = [row, col]
-                    possible_moves = []
+                    possible_moves = []          
+                
         for row in range(8):    
             for col in range(8):
                 symbol = cur_pos[row][col].__str__()
@@ -61,13 +77,11 @@ def play_game():
                     color = (255, 0, 0)
                     board = Start.curPos
                     piece = board[row][col]
-                    if isinstance(piece, Engine.Pieces.piece) :
-                        piece.posMove = []
-                        piece.check(col, row, board)
+                    if isinstance(piece, Engine.Pieces.piece):
                         possible_moves = piece.posMove
                 else:
                     color = LIGHT if (row + col) % 2 == 0 else DARK
-                    pygame.draw.rect(
+                pygame.draw.rect(
                     screen,
                     color,
                     (col * SQUARE_SIZE, row * SQUARE_SIZE,
@@ -88,7 +102,7 @@ def play_game():
                 (0, 255, 0),
                 (c * SQUARE_SIZE + SQUARE_SIZE // 2, r * SQUARE_SIZE + SQUARE_SIZE // 2),
                 SQUARE_SIZE // 4)
-        
+        Played_moves.draw(screen, None)
         pygame.display.flip()
         clock.tick(60)
 def pgn_import():
@@ -100,24 +114,33 @@ def open_data():
     path = "Data.txt"
     os.startfile(path)
 pygame.init()
-res = 720
+res_h = 720
+res_w = 1280
 pygame.display.set_caption("Chess Engine")
 LIGHT = (240, 217, 181)
 DARK  = (181, 136, 99)
+BACKGROUND = (14,14,74)
 font = pygame.font.SysFont("Segoe UI Symbol", 56)
-screen = pygame.display.set_mode((res, res))
-SQUARE_SIZE = res // 8
+font_console = pygame.font.SysFont("console", 34)
+screen = pygame.display.set_mode((res_w, res_h))
+SQUARE_SIZE = 720 // 8 # Size of each square on the chessboard. Rest of the screen for menu UI at 720px right.
 clock = pygame.time.Clock()  
 # Buttons for menu UI
-Start_button = UI.button(play_game, 100, 100, 200, 75, "Play Game")
-Import_PGN_button = UI.button(pgn_import, 100, 200, 200, 75, "Import PGN")
-Open_data_button = UI.button(open_data, 100, 300, 200, 75, "Open training data")
-Exit_button = UI.button(sys.exit, 100, 400, 200, 75, "Exit")
+Start_button = UI.button(play_game, 100, 120, 200, 75, "Play Game")
+Import_PGN_button = UI.button(pgn_import, 100, 220, 200, 75, "Import PGN")
+Open_data_button = UI.button(open_data, 100, 320, 200, 75, "Open training data")
+Settings = UI.button(open_settings,100, 420, 200, 75, "Settings")
+Exit_button = UI.button(sys.exit, 100, 420, 200, 75, "Exit")
+#Menu UI
+Played_moves = UI.TextBox(980,120,250,500,"Previous Moves",(120,120,120),font_console)
+Title = UI.TextBox(res_w//8, res_h//32, res_w//(8/6), 80, "", (255,255,255), font)
+
 def main():
     running = True
     buttons = [Start_button, Exit_button, Import_PGN_button, Open_data_button]
     while running == True:
-        screen.fill((128, 128, 128))
+        screen.fill(BACKGROUND)
+        Title.draw(screen, "-----Welcome to my chess engine-----")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
--- UI.py
+++ UI.py
@@ -1,22 +1,42 @@
 import pygame
 from tkinter import filedialog
 
+class TextBox():
+    def __init__(self, x,y,width,height,title,colour , font, border_colour = (0,0,0)):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.title = title
+        self.colour = colour
+        self.font = font
+        self.border_colour = border_colour
+        self.rect = pygame.Rect(x, y, width, height)
+        self.border = pygame.Rect(x-4, y-4, width+8, height+8)
+        self.header = font.render(self.title, True, (0, 0, 0))
+    def draw(self, screen, content):
+        pygame.draw.rect(screen, self.colour, self.rect)
+        pygame.draw.rect(screen, self.border_colour, self.border, 4)
+        screen.blit(self.header, (self.x-20, self.y-60))
+        data = self.font.render(content, True, (0,0,0))
+        screen.blit(data, (self.x, self.y))
+
 class button:
-    def __init__(self,function, x, y, width, height, text='', color_idle=(255, 255, 255), color_hover=(200, 200, 200), font_size=30):
+    def __init__(self,function, x, y, width, height, text='', colour_idle=(255, 255, 255), colour_hover=(200, 200, 200), font_size=30):
         self.function = function
         self.rect = pygame.Rect(x, y, width, height)
         self.text = text
-        self.color_idle = color_idle
-        self.color_hover = color_hover
+        self.colour_idle = colour_idle
+        self.colour_hover = colour_hover
         self.font = pygame.font.SysFont(None, font_size)
 
     def draw(self, screen):
         mouse_pos = pygame.mouse.get_pos()
         if self.rect.collidepoint(mouse_pos):
-            color = self.color_hover
+            colour = self.colour_hover
         else:
-            color = self.color_idle
-        pygame.draw.rect(screen, color, self.rect, border_radius=5)
+            colour = self.colour_idle
+        pygame.draw.rect(screen, colour, self.rect, border_radius=5)
         if self.text:
             text_surf = self.font.render(self.text, True, (0, 0, 0))
             text_rect = text_surf.get_rect(center=self.rect.center)
